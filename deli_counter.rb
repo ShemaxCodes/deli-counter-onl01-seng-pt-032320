fd3eee7ce7796d541d49356cdb58b3a4c2953742
Jashema Panapa <jcpanapa@gmail.com>
2020-03-30 17:05:33 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,8 @@ def line(katz_deli)
     puts "The line is currently empty."
   else
     position = 1
-    
+    katz_deli.each do |person|
+      puts "The line is currently: #{postion} #{person}"
   end
 end
 
