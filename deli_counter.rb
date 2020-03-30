96881e0770d54f66d87c7373c351030a48e7b368
Jashema Panapa <jcpanapa@gmail.com>
2020-03-30 16:55:04 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,7 +12,7 @@ def take_a_number(katz_deli, name)
   puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
 end  
   
-def now_serving(katz_deli)
+def now_serving
   if katz_deli.size == 0
     puts "The line is currently empty."
   end
