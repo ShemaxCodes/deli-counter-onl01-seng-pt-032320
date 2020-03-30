8c1fdfbdd2c9668ba550a0316839ba3548f3242b
Jashema Panapa <jcpanapa@gmail.com>
2020-03-30 14:41:05 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,14 @@
-# Write your code here.
+def line(katz_deli)
+  if katz_deli.size == 0
+    puts "The line is currently empty."
+  elsif katz_deli.size > 0
+    puts "The line is currently: +=1 #{katz_deli}"
+  end
+end
+
+
+  
+  
+  
+  #other_deli.each { |name| puts "The line is currently #{name}"}
+#end
