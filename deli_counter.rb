d9157f6b9e131350d93f525eb6f9be3c8da4c556
Jashema Panapa <jcpanapa@gmail.com>
2020-03-30 17:29:54 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,7 +8,7 @@ def line(katz_deli)
       other_deli << "#{position}. #{person}"
       position += 1 
   end
-    puts "The line is currently: #{other_deli.join(" ")}"
+    puts "The line is currently: #{other_deli.join("")}"
 end
 
 def take_a_number(katz_deli, name)
