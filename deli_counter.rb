970a8a768c2bdf40312861b04f850f241eccc68f
Jashema Panapa <jcpanapa@gmail.com>
2020-03-30 14:52:35 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,8 +2,6 @@ def line(katz_deli)
   katz_deli = []
   if katz_deli.size == 0
     puts "The line is currently empty."
-  elsif katz_deli.size > 0
-    puts "The line is currently: +=1 #{katz_deli}"
   end
 end
 
